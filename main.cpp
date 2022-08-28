24146faf99cbfad45d809952330d079d31a26aef
bUTKARS <112314293+bUTKARS@users.noreply.github.com>
2022-08-28 16:57:02 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 class Human{
 private:
+	string name;
 	int age;
 public:
-	void displayage(){
-		cout<<age<<endl;
-	}
-	void setAge(int value)
+	Human()//This is a constructor it has the same name as the class
 	{
-		age=value;
+		cout<<"Constructor called here"<<endl;
+		age=22;
+		name="Utkarsh";
+	}
+	void display()
+	{
+		cout<<age<<" "<<name<<endl;
 	}
 };
 int main()
 {
-	Human tichu;
-	tichu.setAge(69);
-	tichu.displayage();
+	Human u;// as soon as you execute this the constructor will be called automatically
+	u.display();
+	return 0;
+	
 }
