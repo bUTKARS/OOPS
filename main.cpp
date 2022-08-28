0e83ec366164a14ffc7014a1d9283e234e9d3975
bUTKARS <112314293+bUTKARS@users.noreply.github.com>
2022-08-28 17:10:41 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,25 +5,14 @@ private:
 	string name;
 	int age;
 public:
-	Human()
+	
+	Human(string s="Utkarsh",int a=33)
 	{
 		cout<<"Constructor called here"<<endl;
-		age=22;
-		name="Utkarsh";
-	}
-	Human(string s)
-	{
-		cout<<"Constructor with string argument"<<'\n';
-		age=22;
 		name=s;
+		age=a;
 	}
-	Human(int s)
-	{
-		cout<<"Constructor with int parameter"<<'\n';
-		age=s;
-		name="tichu";
 
-	}
 	void display()
 	{
 		cout<<age<<" "<<name<<endl;
@@ -31,10 +20,11 @@ public:
 };
 int main()
 {
-	Human u;//will call the default constructor
-	u.display();
-	Human tichu(69);//will call the constructor function which uses integer input as arguent
+	
+	Human tichu("dsfg",22);
+	Human u;//passed with no paramters assigns the default value
 	tichu.display();
+	u.display();
 	return 0;
 	
 }
