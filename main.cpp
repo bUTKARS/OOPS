2e86804e560091bfc29c14967ee2dbad853a6f50
bUTKARS <112314293+bUTKARS@users.noreply.github.com>
2022-08-28 16:58:07 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,24 @@ private:
 	string name;
 	int age;
 public:
-	Human()//This is a constructor it has the same name as the class
+	Human()
 	{
 		cout<<"Constructor called here"<<endl;
 		age=22;
 		name="Utkarsh";
+	}
+	Human(string s)
+	{
+		cout<<"Constructor with string argument"<<'\n';
+		age=22;
+		name=s;
+	}
+	Human(int s)
+	{
+		cout<<"Constructor with int parameter"<<'\n';
+		age=s;
+		name="tichu";
+
 	}
 	void display()
 	{
@@ -18,8 +31,10 @@ public:
 };
 int main()
 {
-	Human u;// as soon as you execute this the constructor will be called automatically
+	Human u;//will call the default constructor
 	u.display();
+	Human tichu(69);//will call the constructor function which uses integer input as arguent
+	tichu.display();
 	return 0;
 	
 }
