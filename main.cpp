07180262bbe8ba040f584f1dc7199e9ad0364256
bUTKARS <112314293+bUTKARS@users.noreply.github.com>
2022-08-28 14:57:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ class HumanBeing{
 public:
 	string name;
 
-	void introduce()
-	{
-		cout<<"hi"<<" "<<name;
-	}
+	void introduce();//You declared a function but not specified it
 };
+void HumanBeing::introduce()//You can declare it out of the scope of class by using scope
+//resolution operator.
+{
+	cout<<HumanBeing::name<<'\n';//to access some element in the class tempalte you need to use 
+	//:: operator again
+}
+// ::->scope resolution operator
 int main()
 {
 	HumanBeing u;
 	u.name="xx";
 	u.introduce();
 	cout<<'\n';
-	HumanBeing *kitchu=new HumanBeing();
-	kitchu->name="tichu";
-	kitchu->introduce();
-	delete kitchu;
-	cout<<'\n';
+	
 		return 0;
 }
