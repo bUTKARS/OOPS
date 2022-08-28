3aaf516e9b7db96703839e5f86eea344469f767c
bUTKARS <112314293+bUTKARS@users.noreply.github.com>
2022-08-28 14:14:59 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class HumanBeing{
+public:
+	string name;
+
+	void introduce()
+	{
+		cout<<"hi"<<" "<<name;
+	}
+};
+int main()
+{
+	HumanBeing u;
+	u.name="xx";
+	u.introduce();
+	return 0;
+}
