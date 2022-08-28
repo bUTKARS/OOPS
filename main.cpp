c21e731919baf824df08678ab3f3d7e13ef2241a
bUTKARS <112314293+bUTKARS@users.noreply.github.com>
2022-08-28 15:10:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-class HumanBeing{
-public:
-	string name;
 
-	void introduce();//You declared a function but not specified it
+class Human{
+private:
+	int age;
+public:
+	void displayage(){
+		cout<<age<<endl;
+	}
+	void setAge(int value)
+	{
+		age=value;
+	}
 };
-void HumanBeing::introduce()//You can declare it out of the scope of class by using scope
-//resolution operator.
-{
-	cout<<HumanBeing::name<<'\n';//to access some element in the class tempalte you need to use 
-	//:: operator again
-}
-// ::->scope resolution operator
 int main()
 {
-	HumanBeing u;
-	u.name="xx";
-	u.introduce();
-	cout<<'\n';
-	
-		return 0;
+	Human tichu;
+	tichu.setAge(69);
+	tichu.displayage();
 }
